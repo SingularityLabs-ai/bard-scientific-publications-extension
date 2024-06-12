11898416e8afcd77031e60c63decaada2f5153f7
ishan dutta <ishandutta2007@gmail.com>
2024-06-13 02:22:18 +0530

    src/copyingscript.sh

---
+++ src/copyingscript.sh
@@ -0,0 +1,115 @@
+mkdir am
+mv ~/Downloads/translated_data/am.txt am/messages.json
+mkdir ca
+mv ~/Downloads/translated_data/ca.txt ca/messages.json
+mkdir el
+mv ~/Downloads/translated_data/el.txt el/messages.json
+mkdir en_US
+mv ~/Downloads/translated_data/en_US.txt en_US/messages.json
+mkdir fa
+mv ~/Downloads/translated_data/fa.txt fa/messages.json
+mkdir gu
+mv ~/Downloads/translated_data/gu.txt gu/messages.json
+mkdir hu
+mv ~/Downloads/translated_data/hu.txt hu/messages.json
+mkdir iw
+mv ~/Downloads/translated_data/iw.txt iw/messages.json
+mkdir lt
+mv ~/Downloads/translated_data/lt.txt lt/messages.json
+mkdir ms
+mv ~/Downloads/translated_data/ms.txt ms/messages.json
+mkdir pt_BR
+mv ~/Downloads/translated_data/pt_BR.txt pt_BR/messages.json
+mkdir sk
+mv ~/Downloads/translated_data/sk.txt sk/messages.json
+mkdir sw
+mv ~/Downloads/translated_data/sw.txt sw/messages.json
+mkdir tr
+mv ~/Downloads/translated_data/tr.txt tr/messages.json
+mkdir zh_TW
+mv ~/Downloads/translated_data/zh_TW.txt zh_TW/messages.json
+mkdir ar
+mv ~/Downloads/translated_data/ar.txt ar/messages.json
+mkdir cs
+mv ~/Downloads/translated_data/cs.txt cs/messages.json
+mkdir en
+mv ~/Downloads/translated_data/en.txt en/messages.json
+mkdir es
+mv ~/Downloads/translated_data/es.txt es/messages.json
+mkdir fi
+mv ~/Downloads/translated_data/fi.txt fi/messages.json
+mkdir he
+mv ~/Downloads/translated_data/he.txt he/messages.json
+mkdir id
+mv ~/Downloads/translated_data/id.txt id/messages.json
+mkdir ja
+mv ~/Downloads/translated_data/ja.txt ja/messages.json
+mkdir lv
+mv ~/Downloads/translated_data/lv.txt lv/messages.json
+mkdir nl
+mv ~/Downloads/translated_data/nl.txt nl/messages.json
+mkdir pt_PT
+mv ~/Downloads/translated_data/pt_PT.txt pt_PT/messages.json
+mkdir sl
+mv ~/Downloads/translated_data/sl.txt sl/messages.json
+mkdir ta
+mv ~/Downloads/translated_data/ta.txt ta/messages.json
+mkdir uk
+mv ~/Downloads/translated_data/uk.txt uk/messages.json
+mkdir bg
+mv ~/Downloads/translated_data/bg.txt bg/messages.json
+mkdir da
+mv ~/Downloads/translated_data/da.txt da/messages.json
+mkdir en_AU
+mv ~/Downloads/translated_data/en_AU.txt en_AU/messages.json
+mkdir es_419
+mv ~/Downloads/translated_data/es_419.txt es_419/messages.json
+mkdir fil
+mv ~/Downloads/translated_data/fil.txt fil/messages.json
+mkdir hi
+mv ~/Downloads/translated_data/hi.txt hi/messages.json
+mkdir in
+mv ~/Downloads/translated_data/in.txt in/messages.json
+mkdir kn
+mv ~/Downloads/translated_data/kn.txt kn/messages.json
+mkdir ml
+mv ~/Downloads/translated_data/ml.txt ml/messages.json
+mkdir no
+mv ~/Downloads/translated_data/no.txt no/messages.json
+mkdir ro
+mv ~/Downloads/translated_data/ro.txt ro/messages.json
+mkdir sr
+mv ~/Downloads/translated_data/sr.txt sr/messages.json
+mkdir te
+mv ~/Downloads/translated_data/te.txt te/messages.json
+mkdir te
+mv ~/Downloads/translated_data/vi.txt vi/messages.json
+mkdir bn
+mv ~/Downloads/translated_data/bn.txt bn/messages.json
+mkdir de
+mv ~/Downloads/translated_data/de.txt de/messages.json
+mkdir en_GB
+mv ~/Downloads/translated_data/en_GB.txt en_GB/messages.json
+mkdir et
+mv ~/Downloads/translated_data/et.txt et/messages.json
+mkdir fr
+mv ~/Downloads/translated_data/fr.txt fr/messages.json
+mkdir hr
+mv ~/Downloads/translated_data/hr.txt hr/messages.json
+mkdir it
+mv ~/Downloads/translated_data/it.txt it/messages.json
+mkdir ko
+mv ~/Downloads/translated_data/ko.txt ko/messages.json
+mkdir mr
+mv ~/Downloads/translated_data/mr.txt mr/messages.json
+mkdir pl
+mv ~/Downloads/translated_data/pl.txt pl/messages.json
+mkdir ru
+mv ~/Downloads/translated_data/ru.txt ru/messages.json
+mkdir sv
+mv ~/Downloads/translated_data/sv.txt sv/messages.json
+mkdir th
+mv ~/Downloads/translated_data/th.txt th/messages.json
+mkdir zh_CN
+mv ~/Downloads/translated_data/zh_CN.txt zh_CN/messages.json
+js-beautify-alljson
